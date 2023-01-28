fa8b1a8f48b9300e215b829d1005bd1f719a868e
Masataka Sawahara <m.sawahara94@gmail.com>
2023-01-29 00:14:09 +0900

    add tcp retransmission timeout setting

--- me56ps2-emulator-rp2040.ino
+++ me56ps2-emulator-rp2040.ino
@@ -4,6 +4,7 @@
 #include <EthernetClient.h>
 #include <EthernetServer.h>
 #include <EthernetUdp.h>
+#include <utility/w5100.h>
 
 #include <algorithm>
 #include <cstdarg>
@@ -254,6 +255,33 @@ void generate_board_mac_address(uint8_t *mac)
     mac[5] = uid.id[2] ^ uid.id[5];
 }
 
+void w5x00_write_uint8(const uint16_t addr, uint8_t value)
+{
+    W5100.write(addr, value);
+}
+
+void w5x00_write_uint16(const uint16_t addr, uint16_t value)
+{
+    uint8_t buf[2];
+    buf[0] = value >> 8;
+    buf[1] = value & 0xff;
+    W5100.write(addr, buf, 2);
+}
+
+void set_retry_register(const uint16_t retry_time_value, const uint8_t retry_count)
+{
+    switch (Ethernet.hardwareStatus()) {
+        case EthernetW5100:
+            w5x00_write_uint16(0x0017, retry_time_value);
+            w5x00_write_uint8(0x0019, retry_count);
+            break;
+        case EthernetW5500:
+            w5x00_write_uint16(0x0019, retry_time_value);
+            w5x00_write_uint8(0x001b, retry_count);
+            break;
+    }
+}
+
 void initialize_network(void)
 {
     using namespace config;
@@ -274,6 +302,8 @@ void initialize_network(void)
         using namespace static_ip;
         Ethernet.begin(mac_addr, ip_addr, dns_server, gateway, subnet_mask);
     }
+
+    set_retry_register(retry_time_value, retry_count);
 }
 
 void setup1()
